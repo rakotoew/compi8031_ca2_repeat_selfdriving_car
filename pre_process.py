434785a45121b3e6a373b7451820e53a434f0168
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-09-03 00:34:05 +0200

    small fix from previous commit

--- pre_process.py
+++ pre_process.py
@@ -129,7 +129,11 @@ def random_shadow(image_to_augment):
     b = np.random.uniform(45, 115)
     a = (image_to_augment.shape[0] // 2 - b) / (image_to_augment.shape[1] // 2)
     above = np.random.rand() > 0.5
-
+    for x in range(image_shadow.shape[1]):
+        for y in range(image_shadow.shape[0]):
+            if (a * x + b < y and above) or (a * x + b > y and not above):
+                image_shadow[y, x] = image_shadow[y, x] * 0.4
+    return image_shadow
 
 def random_augment(image_to_augment, steering_angle):
     augment_image = mpimg.imread(image_to_augment)
@@ -144,11 +148,7 @@ def random_augment(image_to_augment, steering_angle):
     return augment_image, steering_angle
 
 
-    for x in range(image_shadow.shape[1]):
-        for y in range(image_shadow.shape[0]):
-            if (a * x + b < y and above) or (a * x + b > y and not above):
-                image_shadow[y, x] = image_shadow[y, x] * 0.4
-    return image_shadow
+
 
 
 def batch_generator(image_paths, steering_ang, batch_size, is_training):
